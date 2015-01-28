1e7caf10bd78dfdddf22c47774110c5052453f7a
axton21 <axton_n@yahoo.com>
2015-01-28 15:58:59 -0500

    fizzbuzzed outtahere

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,24 @@
 # code goes here
+def fizzbuzz(int)
+    if int % 3 == 0
+        if int % 5 ==0
+            "FizzBuzz"
+        else
+            "Fizz"
+        end
+
+    elsif int % 5 == 0
+        if int % 3 == 0
+            "FizzBuzz"
+        else
+            "Buzz"
+        end
+
+    end
+end
+
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(10)
+fizzbuzz(30)
